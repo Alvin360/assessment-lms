e378a3a0e8470a4a9e1d03dd363e9328a3d7d36b
Pauline Ann Bautista <paulineannbautista@gmail.com>
2024-07-14 22:29:49 +0800

    added nav bar

--- PROFESSOR/index.php
+++ PROFESSOR/index.php
@@ -21,6 +21,38 @@
 </head>
 
 <body>
+    <!-- Header section containing the navigation bars -->
+    <header>
+        <div class="top-bar">
+            <div class="logo">
+                <img src="logo.png" alt="PUP">
+            </div>
+            <nav class="main-nav">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Dashboard</a></li>
+                    <li><a href="#">My courses</a></li>
+                    <li><a href="../PROFESSOR/index.php">Assessment</a></li>
+                </ul>
+            </nav>
+            <nav class="profile-nav">
+                <ul>
+                    <li><a href="#">Notification</a></li>
+                    <li><a href="#">Messages</a></li>
+                    <li><a href="#">Profile</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="bottom-bar">
+            <ul>
+                <li><a href="#">Course</a></li>
+                <li><a href="#">Participants</a></li>
+                <li><a href="#">Grades</a></li>
+                <li><a href="#">Competencies</a></li>
+            </ul>
+        </div>
+    </header>
+
     <div id="landing_page">
         <div class="flex_row">
             <button onclick="window.location.href='../PROFESSOR/pages/create_assessment.html'">+ Add Assessment</button>
