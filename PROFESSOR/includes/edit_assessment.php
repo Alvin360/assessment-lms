d79b496d7a7208231a869708714c67f4a74bf524
Pauline Ann Bautista <paulineannbautista@gmail.com>
2024-07-15 00:00:50 +0800

    added nav bar

--- PROFESSOR/includes/edit_assessment.php
+++ PROFESSOR/includes/edit_assessment.php
@@ -61,6 +61,38 @@ function fetchCorrectAnswers($questionID, $questionType, $conn) {
 
 </head>
 <body>
+    <!-- Header section containing the navigation bars -->
+    <header>
+        <div class="top-bar">
+            <div class="logo">
+                <img src="logo.png" alt="PUP">
+            </div>
+            <nav class="main-nav">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Dashboard</a></li>
+                    <li><a href="#">My courses</a></li>
+                    <li><a href="../index.php">Assessment</a></li>
+                </ul>
+            </nav>
+            <nav class="profile-nav">
+                <ul>
+                    <li><a href="#">Notification</a></li>
+                    <li><a href="#">Messages</a></li>
+                    <li><a href="#">Profile</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="bottom-bar">
+            <ul>
+                <li><a href="#">Course</a></li>
+                <li><a href="#">Participants</a></li>
+                <li><a href="#">Grades</a></li>
+                <li><a href="#">Competencies</a></li>
+            </ul>
+        </div>
+    </header>
+
     <h2>Edit Assessment</h2>
     <form action="update_assessment.php" method="post">
         <input type="hidden" name="assessmentID" value="<?php echo $assessmentID; ?>">
