4f64472870e767a05371e536511574566af705d5
Alloy <alloysius86@gmail.com>
2024-07-15 21:45:18 +0800

    Added Essay type on create_assessment, changed matching type from dropdown to input text

--- PROFESSOR/includes/save_assessment.php
+++ PROFESSOR/includes/save_assessment.php
@@ -16,19 +16,18 @@ $noOfItems = is_array($questions) ? count($questions) : 0;
 $assessmentID = uniqid('A');
 $date = date('Y-m-d H:i:s');
 
-
 $sql = "INSERT INTO ASSESSMENT (assessment_ID, assessment_Name,  date_Created, open_date, creator_ID, subject_Code, assessment_Type, time_Limit, no_Of_Items, closing_date, assessment_Desc, allowed_Attempts) 
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param('sssssssssssi', $assessmentID, $assessmentName, $date, $openDate, $creatorID, $subjectCode, $assessmentType, $timeLimit, $noOfItems, $closingDate, $assessmentDesc, $allowedAttempts);
 
 if ($stmt->execute()) {
-    $insertQuestionSql = "INSERT INTO EXAMINATION_BANK (assessment_ID, question_ID, question_No, question, points, question_Type, choice1, choice2, choice3, choice4, boolean, fill_Blank, match1, match2, match3, match4, match5, match6, match7, match8, match9, match10) 
-                          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $insertQuestionSql = "INSERT INTO EXAMINATION_BANK (assessment_ID, question_ID, question_No, question, points, question_Type, choice1, choice2, choice3, choice4, boolean, fill_Blank, match1, match2, match3, match4, match5, match6, match7, match8, match9, match10, long_Answer) 
+                          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $stmtInsertQuestion = $conn->prepare($insertQuestionSql);
 
-    $insertAnswerSql = "INSERT INTO EXAM_ANSWER (assessment_ID, question_ID, answer, m_Ans1, m_Ans2, m_Ans3, m_Ans4, m_Ans5, m_Ans6, m_Ans7, m_Ans8, m_Ans9, m_Ans10) 
-                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $insertAnswerSql = "INSERT INTO EXAM_ANSWER (assessment_ID, question_ID, answer, m_Ans1, m_Ans2, m_Ans3, m_Ans4, m_Ans5, m_Ans6, m_Ans7, m_Ans8, m_Ans9, m_Ans10, long_Answer) 
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $stmtInsertAnswer = $conn->prepare($insertAnswerSql);
 
     if (is_array($questions)) {
@@ -59,7 +58,10 @@ if ($stmt->execute()) {
             $match9 = isset($question['match'][8]) ? $question['match'][8] : null;
             $match10 = isset($question['match'][9]) ? $question['match'][9] : null;
 
-            $stmtInsertQuestion->bind_param('siisssssssssssssssssss', $assessmentID, $questionID, $questionNo, $questionText, $points, $questionType, $choice1, $choice2, $choice3, $choice4, $boolean, $fillBlank, $match1, $match2, $match3, $match4, $match5, $match6, $match7, $match8, $match9, $match10);
+            // Long Answer
+            $longAnswer = isset($question['longAnswer']) ? $question['longAnswer'] : null;
+
+            $stmtInsertQuestion->bind_param('siissssssssssssssssssss', $assessmentID, $questionID, $questionNo, $questionText, $points, $questionType, $choice1, $choice2, $choice3, $choice4, $boolean, $fillBlank, $match1, $match2, $match3, $match4, $match5, $match6, $match7, $match8, $match9, $match10, $longAnswer);
 
             if (!$stmtInsertQuestion->execute()) {
                 echo "Error inserting question: " . $stmtInsertQuestion->error;
@@ -78,7 +80,7 @@ if ($stmt->execute()) {
             $m_Ans9 = isset($question['m_Ans9']) ? $question['m_Ans9'] : null;
             $m_Ans10 = isset($question['m_Ans10']) ? $question['m_Ans10'] : null;
 
-            $stmtInsertAnswer->bind_param('sisssssssssss', $assessmentID, $questionID, $correctAnswer, $m_Ans1, $m_Ans2, $m_Ans3, $m_Ans4, $m_Ans5, $m_Ans6, $m_Ans7, $m_Ans8, $m_Ans9, $m_Ans10);
+            $stmtInsertAnswer->bind_param('sissssssssssss', $assessmentID, $questionID, $correctAnswer, $m_Ans1, $m_Ans2, $m_Ans3, $m_Ans4, $m_Ans5, $m_Ans6, $m_Ans7, $m_Ans8, $m_Ans9, $m_Ans10, $longAnswer);
 
             if (!$stmtInsertAnswer->execute()) {
                 echo "Error inserting correct answer: " . $stmtInsertAnswer->error;
@@ -95,6 +97,6 @@ if ($stmt->execute()) {
 
 $stmt->close();
 $stmtInsertQuestion->close();
-$stmtInsertAnswer->close();
+stmtInsertAnswer->close();
 $conn->close();
 ?>
