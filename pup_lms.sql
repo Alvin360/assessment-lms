33f897760e6a17cbd0a494cdffbde8a1c05feb2e
Pauline Ann Bautista <paulineannbautista@gmail.com>
2024-07-16 21:26:30 +0800

    updated sql code

--- pup_lms.sql
+++ pup_lms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3307:3307
--- Generation Time: Jul 14, 2024 at 09:38 AM
+-- Generation Time: Jul 16, 2024 at 03:24 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -39,16 +39,28 @@ CREATE TABLE `assessment` (
   `no_Of_Items` varchar(3) DEFAULT NULL,
   `closing_Date` datetime NOT NULL,
   `assessment_Desc` varchar(200) NOT NULL,
-  `allowed_Attempts` int(11) NOT NULL
+  `allowed_Attempts` int(11) NOT NULL,
+  `is_Archived` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `assessment`
 --
 
-INSERT INTO `assessment` (`assessment_ID`, `assessment_Name`, `date_Created`, `open_Date`, `creator_ID`, `subject_Code`, `assessment_Type`, `time_Limit`, `no_Of_Items`, `closing_Date`, `assessment_Desc`, `allowed_Attempts`) VALUES
-('A669361683', 'Quiz 1: Auditing Theories', '2024-07-14 07:26:00', '2024-07-01 13:00:00', '201510754MN0', 'ACCO123', 'M', '00:50:00', '2', '2024-07-01 14:00:00', 'Select the correct answer', 1),
-('A669361ee8', 'Quiz 1: HTML', '2024-07-14 07:28:14', '2024-06-01 12:00:00', '201510754MN0', 'COMP124', 'M', '00:30:00', '1', '2024-06-01 23:59:00', 'Select the best answer', 1);
+INSERT INTO `assessment` (`assessment_ID`, `assessment_Name`, `date_Created`, `open_Date`, `creator_ID`, `subject_Code`, `assessment_Type`, `time_Limit`, `no_Of_Items`, `closing_Date`, `assessment_Desc`, `allowed_Attempts`, `is_Archived`) VALUES
+('A669361683', 'Quiz 1: Auditing Theories', '2024-07-14 07:26:00', '2024-07-01 13:00:00', '201510754MN0', 'ACCO123', 'M', '00:50:00', '2', '2024-07-01 14:00:00', 'Select the correct answer', 1, 0),
+('A669361ee8', 'Quiz 1: HTML', '2024-07-14 00:00:00', '2024-06-01 12:00:00', '201510754MN0', 'COMP124', 'Q', '00:30:00', '1', '2024-06-01 13:30:00', '', 1, 1),
+('A6693a1468', 'Quiz 2: CSS', '2024-07-14 00:00:00', '2024-06-04 12:00:00', '201510754MN0', 'COMP124', 'Q', '00:50:00', '1', '2024-06-04 12:50:00', 'Choose the best answer', 1, 1),
+('A6693a9e1d', 'Quiz 1: Agents of AI', '2024-07-14 00:00:00', '2024-07-10 16:00:00', '201510754MN0', 'COSC123', 'Q', '01:00:00', '1', '2024-07-13 16:00:00', '', 1, 0),
+('A6693b9797', 'Quiz 2: Searching Algorithm', '2024-07-14 00:00:00', '2024-07-10 19:39:00', '201510754MN0', 'COSC123', 'Q', '00:30:00', '6', '2024-07-17 19:39:00', 'Choose the best answer', 1, 0),
+('A6693d7155', 'Quiz 3 CSS', '2024-07-14 00:00:00', '2024-07-13 21:47:00', '201510754MN0', 'COSC123', 'Q', '01:00:00', '1', '2024-07-16 21:47:00', '', 1, 0),
+('A6693d7e81', 'Quiz 4', '2024-07-14 00:00:00', '2024-07-13 21:51:00', '201510754MN0', 'COSC123', 'Q', '01:00:00', '1', '2024-07-18 21:51:00', '', 5, 1),
+('A6693db177', 'Quiz 5', '2024-07-14 16:05:11', '2024-07-13 21:59:00', '201510754MN0', 'COMP123INS', 'Q', '01:00:00', '4', '2024-07-18 21:59:00', 'Select the best answer', 5, 1),
+('A6693eb247', 'Quiz 1', '2024-07-14 17:13:40', '2024-07-01 23:13:00', '201510754MN0', 'COMP123INS', 'Q', '00:30:00', '1', '2024-07-16 23:13:00', 'choose the best answer', 2, 0),
+('A6693fa40e', 'Quiz 100', '2024-07-14 18:18:08', '2024-07-14 00:17:00', '201510754MN0', 'COMP124', 'M', '01:00:00', '1', '2024-07-17 00:17:00', '21', 2, 1),
+('A66941f998', 'Quiz 5: JSON', '2024-07-15 02:57:29', NULL, '201510754MN0', NULL, 'Q', '05:00:00', '1', '0000-00-00 00:00:00', 'create a code for the following', 1, 0),
+('A6694872e0', 'Quiz 2: Auditing Problems', '2024-07-15 10:19:26', '2024-07-15 10:00:00', '201510754MN0', 'ACCO123', 'Q', '03:00:00', '1', '2024-07-16 10:00:00', 'Solve the ff. problem. ', 5, 0),
+('A669488710', 'Quiz: Matching Type', '2024-07-15 10:24:49', '2024-07-15 07:00:00', '201510754MN0', 'COSC123', 'M', '00:30:00', '1', '2024-07-17 07:00:00', 'Match the correct answer', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -180,7 +192,26 @@ CREATE TABLE `examination_bank` (
 INSERT INTO `examination_bank` (`assessment_ID`, `question_ID`, `question_No`, `question`, `points`, `question_Type`, `choice1`, `choice2`, `choice3`, `choice4`, `boolean`, `fill_Blank`, `match1`, `match2`, `match3`, `match4`, `match5`, `match6`, `match7`, `match8`, `match9`, `match10`) VALUES
 ('A669361683', 1, 1, 'Which of the following is the primary objective of an audit?', 1, 'M', ' To detect fraud and irregularities.', 'To express an opinion on the fairness of the financial statements.', 'To ensure compliance with all laws and regulations.', 'To provide assurance on the efficiency and effectiveness of operations.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 ('A669361683', 2, 2, 'What is the primary difference between internal and external auditors?', 1, 'M', 'Internal auditors report to management, while external auditors report to shareholders.', 'Internal auditors focus on financial statements, while external auditors focus on internal controls.', 'Internal auditors are required by law, while external auditors are voluntary.', 'Internal auditors are certified public accountants, while external auditors are not.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('A669361ee8', 1, 1, 'What is the meaning of HTML', 1, 'S', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+('A669361ee8', 1, 1, 'What is the meaning of HTML', 1, 'S', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693a1468', 1, 1, 'What is the meaning of CSS', 1, 'M', 'Cascading Style Sheets', 'Creative Style Sheets', 'Cascading Style System', 'Creative Style System', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693a9e1d', 1, 1, 'What are the agents of AI?', 1, 'S', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693b9797', 1, 1, 'Linear search is faster than binary search for sorted data.', 1, 'T', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693b9797', 2, 2, 'Binary search requires the data to be sorted.', 1, 'T', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693b9797', 3, 3, 'Hash tables provide constant-time average lookup, insertion, and deletion operations.', 1, 'T', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693b9797', 4, 4, 'Interpolation search is more efficient than binary search for uniformly distributed data.', 1, 'T', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693b9797', 5, 5, 'Jump search is suitable for large datasets where the elements are sorted.', 1, 'T', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693b9797', 6, 6, 'Hash tables are immune to collisions.', 1, 'T', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693d7155', 1, 1, 'What is the meaning of CSS', 1, 'M', 'Cascading Style Sheets', 'Creative Style Sheets', 'Cascading Style System', 'Creative Style System', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693d7e81', 1, 1, 'What are the agents of AI?', 1, 'M', 'Cascading Style Sheets', 'Creative Style Sheets', 'Cascading Style System', 'Creative Style System', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693db177', 1, 1, 'What is the meaning of metadata?', 1, 'S', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693db177', 2, 2, 'Metadata is the dictionary of data', 1, 'T', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693db177', 3, 3, 'Hash tables provide constant-time average lookup, insertion, and deletion operations.', 1, 'M', 'a', 'b', 'c', 'd', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693db177', 4, 4, 'Match this', 1, 'F', NULL, NULL, NULL, NULL, NULL, NULL, '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'),
+('A6693eb247', 1, 1, 'Question 1', 1, 'M', '1', '2', '3', '4', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693fa40e', 1, 1, 'true', 1, 'T', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A66941f998', 1, 1, 'Create login page', 1000, 'S', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6694872e0', 1, 1, 'You are auditing a manufacturing company that uses the weighted average cost method for inventory valuation. During your audit procedures, you discover that the company has experienced a significant i', 50, 'S', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A669488710', 1, 1, 'Match the following', 10, 'F', NULL, NULL, NULL, NULL, NULL, NULL, 'Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10');
 
 -- --------------------------------------------------------
 
@@ -209,9 +240,27 @@ CREATE TABLE `exam_answer` (
 --
 
 INSERT INTO `exam_answer` (`assessment_ID`, `question_ID`, `answer`, `m_Ans1`, `m_Ans2`, `m_Ans3`, `m_Ans4`, `m_Ans5`, `m_Ans6`, `m_Ans7`, `m_Ans8`, `m_Ans9`, `m_Ans10`) VALUES
-('A669361683', 1, '2', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('A669361683', 2, '1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('A669361ee8', 1, 'H', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+('A669361683', 1, '1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A669361683', 2, 'T', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A669361ee8', 1, '1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693a1468', 1, '1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693a9e1d', 1, '1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693b9797', 1, '1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693b9797', 2, 'T', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693b9797', 3, 'T', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693b9797', 4, 'T', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693b9797', 5, 'T', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693b9797', 6, 'F', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693d7155', 1, '1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693d7e81', 1, '1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693db177', 1, '1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693db177', 2, 'T', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693db177', 3, '1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6693db177', 4, NULL, '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'),
+('A6693eb247', 1, '1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A66941f998', 1, '1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A6694872e0', 1, '1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('A669488710', 1, '1', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10');
 
 -- --------------------------------------------------------
 
@@ -358,6 +407,28 @@ CREATE TABLE `user_examination` (
   `grade` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `user_examination`
+--
+
+INSERT INTO `user_examination` (`user_ID`, `assessment_ID`, `date_Start`, `time_Start`, `date_End`, `time_End`, `score`, `grade`) VALUES
+(NULL, NULL, NULL, NULL, '2024-07-14', '13:32:43', '0', 0),
+('202110755MN0', 'A6693a9e1d', NULL, NULL, '2024-07-14', '13:33:26', '1', 100),
+('202110755MN0', NULL, NULL, NULL, '2024-07-14', '13:50:02', '0', 0),
+('202110755MN0', 'A6693b9797', NULL, NULL, '2024-07-14', '13:56:25', '4', 66.6667),
+('202110755MN0', 'A6693d7155', NULL, NULL, '2024-07-14', '15:48:30', '1', 100),
+('202110755MN0', 'A6693d7e81', NULL, NULL, '2024-07-14', '15:52:45', '0', 0),
+('202110755MN0', 'A6693db177', NULL, NULL, '2024-07-15', '03:09:11', '1.2', 30),
+('202110755MN0', 'A6694872e0', NULL, NULL, '2024-07-15', '20:10:04', '0', 0),
+('202110755MN0', 'A6694872e0', NULL, NULL, '2024-07-15', '20:11:06', '0', 0),
+(NULL, 'A6694872e0', NULL, NULL, '2024-07-15', '20:51:13', '0', 0),
+(NULL, 'A6694872e0', NULL, NULL, '2024-07-15', '20:51:15', '0', 0),
+(NULL, 'A6694872e0', NULL, NULL, '2024-07-15', '20:53:17', '0', 0),
+('202120322MN0', 'A6694872e0', NULL, NULL, '2024-07-15', '20:54:23', '0', 0),
+('202120322MN0', 'A6694872e0', NULL, NULL, '2024-07-15', '21:08:43', '0', 0),
+('202110755MN0', 'A6693d7e81', NULL, NULL, '2024-07-15', '21:13:08', '0', 0),
+('202110755MN0', 'A6693d7e81', NULL, NULL, '2024-07-15', '21:50:53', '0', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -373,6 +444,22 @@ CREATE TABLE `user_exam_report` (
   `subject_Code` varchar(10) DEFAULT NULL,
   `date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_exam_report`
+--
+
+INSERT INTO `user_exam_report` (`user_ID`, `assessment_ID`, `attempt_Number`, `score`, `grade`, `subject_Code`, `date`) VALUES
+('202110345MN0', 'A669361ee8', 1, '1', 1, 'COMP124', '2024-06-01'),
+('202110755MN0', 'A6693a9e1d', 1, '1', 100, 'COSC123', '2024-07-14'),
+('202110755MN0', 'A6693b9797', 1, '4', 66.6667, 'COSC123', '2024-07-14'),
+('202110755MN0', 'A6693d7155', 1, '1', 100, 'COSC123', '2024-07-14'),
+('202110755MN0', 'A6693d7e81', 1, '0', 0, 'COSC123', '2024-07-14'),
+('202110755MN0', 'A6693db177', 1, '1.2', 30, 'COMP123INS', '2024-07-15'),
+('202120322MN0', 'A6694872e0', 1, '0', 0, 'ACCO123', '2024-07-15'),
+('202120322MN0', 'A6694872e0', 2, '0', 0, 'ACCO123', '2024-07-15'),
+('202110755MN0', 'A6693d7e81', 2, '0', 0, 'COSC123', '2024-07-15'),
+('202110755MN0', 'A6693d7e81', 3, '0', 0, 'COSC123', '2024-07-15');
 
 -- --------------------------------------------------------
 
@@ -672,16 +759,6 @@ ALTER TABLE `user_exam_report`
 ALTER TABLE `user_role`
   ADD CONSTRAINT `user_role_ibfk_1` FOREIGN KEY (`user_ID`) REFERENCES `user_information` (`user_ID`);
 COMMIT;
-
-
--- ADD ESSAY TYPE QUESTION
---
-
-ALTER TABLE `examination_bank`
-ADD COLUMN `long_Answer` TEXT NULL AFTER `choice4`;
-
-ALTER TABLE `exam_answer`
-ADD COLUMN `long_Answer` TEXT NULL AFTER `m_Ans10`;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
